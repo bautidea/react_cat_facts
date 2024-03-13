050db4da4287245a21f18b282dd1d3ae9c6889d3
bautidea <juanbautistadeangelis@gmail.com>
2024-03-13 09:54:19 -0300

    creating data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
-const App = () => {
-  return <h1>Hi World :D</h1>;
-};
+import { useEffect, useState } from 'react'
 
-export default App;
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE = `https://cataas.com/cat/says/${firstWord}?json=true`
+
+const App = () => {
+  const [fact, setFact] = useState()
+  const [imgUrl, setImgUrl] = useState()
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then((response) => response.json())
+      .then((data) =>{
+        const { fact } = data
+
+        setFact(fact)
+
+        const firstWords = fact.split(' ').slice(0, 3).join(' ')
+
+        fetch(`https://cataas.com/cat/says/${firstWords}?json=true`)
+          .then((response) => response.json())
+          .then((imgRes => {
+            console.log(imgRes)
+          }))
+      })
+  }, [])
+
+  return (
+    <main>
+      <h1>Scroll Cat App</h1>
+      {fact && <p>{fact}</p>}
+      {imgUrl && <img src={imgUrl} alt={`Image of a cat with the first three words of ${fact}`}/>}
+    </main>
+  )
+}
+
+export default App
